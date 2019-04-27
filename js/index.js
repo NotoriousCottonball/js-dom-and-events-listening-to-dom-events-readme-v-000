46473e8e7c11157af69f54c4c53cd2e92f13d367
George  Pianka <gpianka1@gmail.com>
2019-04-27 13:58:10 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -9,5 +9,5 @@ main.addEventListener('click', function(event) {
 const input = document.querySelector('input');
  
 input.addEventListener('keydown', function(e) {
-  console.log(e.which);
+  alert('I was clicked!');
 });
