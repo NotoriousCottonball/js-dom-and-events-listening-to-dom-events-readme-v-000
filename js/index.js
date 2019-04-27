0b495072a05d59305614f2634611033a8fc27fd5
George  Pianka <gpianka1@gmail.com>
2019-04-27 14:08:40 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -24,13 +24,3 @@ input.addEventListener('keydown', function(e) {
   }
 });
 
-const input = document.querySelector('input');
- 
-input.addEventListener('keydown', function(e) {
-  if (e.which === 71) {
-    console.log('default prevented');
-    return e.preventDefault();
-  } else {
-    console.log('Not a "g"');
-  }
-});
